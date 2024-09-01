09ce6a0e1f3527d4ce28bfba7e679141cbce2f6e
jaggub529 <jaggub529@gmail.com>
2024-08-31 17:44:23 -0700

    4th commit

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import MenuPage from './MenuPage';
 const App = () => {
   return (
     <div>
-      <h1>Welcome to My Restaurant Menu App</h1>
-      <p>This is the main content of the app.</p>
+      <MenuPage />
     </div>
   );
 };
